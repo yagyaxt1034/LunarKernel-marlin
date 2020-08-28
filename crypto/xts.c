cbd3a5e47febad09b6c4cc11abaec7b681aa9448
Yagya Parihar <yagya.parihar@gmail.com>
2020-08-28 07:04:14 -0600

    crypto: Remove definition of free since the compiler already has it

--- crypto/xts.c
+++ crypto/xts.c
@@ -334,12 +334,6 @@ out_put_alg:
 	return inst;
 }
 
-static void free(struct crypto_instance *inst)
-{
-	crypto_drop_spawn(crypto_instance_ctx(inst));
-	kfree(inst);
-}
-
 static struct crypto_template crypto_tmpl = {
 	.name = "xts",
 	.alloc = alloc,
